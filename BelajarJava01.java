f2a40eef84327f66e8f27a9e573fb73c3998c279
Najwa226 <najwashofaimama@gmail.com>
2024-09-21 11:31:31 +0700

    Add files via upload

---
+++ BelajarJava01.java
@@ -0,0 +1,13 @@
+class BelajarJava01{
+    public static void main (String[] args) { 
+        System.out.println("Nama Saya Najwa");
+        System.out.println("NIM : 607062400001");
+        System.out.println("Kelas D3IF - 48 01");
+    }
+}
+
+
+
+
+
+   
